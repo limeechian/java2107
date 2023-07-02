07d9d8637874837b0e4e09c76d2c9fbc6aef13f2
limeechian <limeechian0211@gmail.com>
2023-07-03 02:04:48 +0800

    JAVA 2107 - Hotel Room Booking System

---
+++ src/java2107/BookingStatus.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java2107;
+
+
+public class BookingStatus {
+    public static String status;  // Public variable to store the status
+
+    // Define constants for the status values
+    public static final String STATUS_NEW = "New Booking";
+    public static final String STATUS_OLD = "Old Booking";
+}
